5fb24dcceb6f5b0c40d8afbeb605084a542525ba
E. Grant Kwaago <elijahgrantkwaago@gmail.com>
2023-01-20 11:07:27 +0100

    fix bugs

--- Script.js
+++ Script.js
@@ -5,12 +5,3 @@ function dis(){
     alert("I don't feel so good.")
     document.getElementById("img").style.visibility="hidden";
 }
-function thun(){
-    alert("GOD OF THUNDER");
-    var audio = new audio('https://www.youtube.com/watch?v=T-BOPr7NXME&ab_channel=MrSoundtabel')
-    audio.thun();
-}
-function nerd(){
-    alert("Nerd")
-    location.replace("https://www.youtube.com/watch?v=qz0aGYrrlhU&ab_channel=ProgrammingwithMosh")
-}
