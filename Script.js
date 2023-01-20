f92322981fe9760be9f8365caa11c5c46dd6c173
E. Grant Kwaago <elijahgrantkwaago@gmail.com>
2023-01-20 09:58:29 +0100

    init

---
+++ Script.js
@@ -0,0 +1,16 @@
+function myfunc(){
+    alert("THE ONLY ONE");
+}
+function dis(){
+    alert("I don't feel so good.")
+    document.getElementById("img").style.visibility="hidden";
+}
+function thun(){
+    alert("GOD OF THUNDER");
+    var audio = new audio('https://www.youtube.com/watch?v=T-BOPr7NXME&ab_channel=MrSoundtabel')
+    audio.thun();
+}
+function nerd(){
+    alert("Nerd")
+    location.replace("https://www.youtube.com/watch?v=qz0aGYrrlhU&ab_channel=ProgrammingwithMosh")
+}
